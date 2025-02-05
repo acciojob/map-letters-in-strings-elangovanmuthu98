0788fb9a2f005f5ea56a445a4e92abaf3b8b8eca
elangovanmuthu98 <125250983+elangovanmuthu98@users.noreply.github.com>
2025-02-05 10:54:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let str=prompt();
+let obj={};
+let n=str.length;
+for(int i=0;i<n;i++)
+{
+	if(obj.str.charAt(i).length!=0)
+	{
+		obj.str.charAt(i).push(i);
+	}
+	else{
+	obj.str.charAt(i)=[i];
+	}
+}
+console.log(obj);
